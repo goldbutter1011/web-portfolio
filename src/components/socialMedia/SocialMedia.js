c83abdc27efd68827f09d01ec3a26fcdedc5586c
luckybutter <133196673+luckybutter322982@users.noreply.github.com>
2023-09-14 16:00:58 +0900

    first commit

---
+++ src/components/socialMedia/SocialMedia.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./SocialMedia.css";
+
+
+
+export default function socialMedia(props) {
+  return (
+    <div className="social-media-div">
+      
+    </div>
+  );
+}
---
+++ src/containers/greeting/Greeting.js
@@ -0,0 +1,57 @@
+import React from "react";
+import "./Greeting.css";
+import Button from "../../components/button/Button";
+import { greeting, contactPageData } from "../../portfolio";
+import { Fade } from "react-reveal";
+import FeelingProud from "./FeelingProud";
+
+const ContactData = contactPageData.contactSection;
+export default function Greeting(props) {
+  const theme = props.theme;
+  return (
+    <Fade bottom duration={2000} distance="40px">
+      <div className="greet-main" id="greeting">
+        <div className="greeting-main">
+          <div className="greeting-text-div">
+            <div>
+              <div className="contact-heading-img-div">
+                <img
+                  src={require(`../../assests/images/${ContactData["profile_image_path"]}`)}
+                  alt=""
+                />
+              </div>
+              <h1 className="greeting-text" style={{ color: theme.text }}>
+                {greeting.title}
+              </h1>
+              {greeting.nickname && (
+                <h2 className="greeting-nickname" style={{ color: theme.text }}>
+                  ( {greeting.nickname} )
+                </h2>
+              )}
+              <p
+                className="greeting-text-p subTitle"
+                style={{ color: theme.secondaryText }}
+              >
+                {greeting.subTitle}
+              </p>
+              <div className="portfolio-repo-btn-div">
+                <Button
+                  text="⭐ Star Me On Github"
+                  newTab={true}
+                  href={greeting.portfolio_repository}
+                  theme={theme}
+                  className="portfolio-repo-btn"
+                />
+              </div>
+              {/* <div className="button-greeting-div">
+              <Button text="Contact me" href="#contact" />
+              <Button text="See my resume" newTab={true} href={greeting.resumeLink} />
+            </div> */}
+            </div>
+          </div>
+          
+        </div>
+      </div>
+    </Fade>
+  );
+}
